8dc958ff06b84a73fcd6beb368bc0ff8a0768bf4
youngjun <yj0913k@naver.com>
2022-12-09 23:06:12 +0900

    1209 프로젝트 생성

---
+++ src/main/java/com/example/junproject/JunProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.junproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunProjectApplication.class, args);
+	}
+
+}
